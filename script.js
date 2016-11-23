b2d1f860017576ba0e36ba3dbfd88a648e9df3c5
uttariya <uttariya@outlook.com>
2016-11-23 12:55:05 +0530

    ball moving in an enclosed box.perpetual motion

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-
+var startx=50;
+var starty=50;
+var dx=2;
+var dy=-2;
+function drawball()
+{
+startx=startx+dx;
+starty=starty+dy;
 ctx.beginPath();
-ctx.rect(20,40,50,50);
-ctx.fillStyle="#f00";
+ctx.arc(startx, starty, 10, 0, Math.PI*2);
+ctx.fillStyle = "#0095DD";
 ctx.fill();
 ctx.closePath();
-
-ctx.beginPath();
-ctx.arc(240,160,20,0,Math.PI*2,false);
-ctx.fillStyle="green";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle= 'rgba(0,0,255,0.5)';
-ctx.stroke();
-ctx.closePath();
+}
+function draw() {
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+dx=startx<=20||startx>=460?dx*(-1):dx;
+dy=starty<=20||starty>=300?dy*(-1):dy;
+drawball();
+}
+setInterval(draw,10);
