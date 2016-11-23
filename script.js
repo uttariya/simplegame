972630a9e7ea3501d8603d04285ca56cbff66bfc
uttariya <uttariya@outlook.com>
2016-11-23 18:20:56 +0530

    added paddle below and added eventselectors

--- script.js
+++ script.js
@@ -1,23 +1,63 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var startx=50;
-var starty=50;
+var x=50;
+var y=50;
 var dx=2;
 var dy=-2;
+var radius=10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
 function drawball()
 {
-startx=startx+dx;
-starty=starty+dy;
+x=x+dx;
+y=y+dy;
 ctx.beginPath();
-ctx.arc(startx, starty, 10, 0, Math.PI*2);
+ctx.arc(x, y, radius, 0, Math.PI*2);
 ctx.fillStyle = "#0095DD";
 ctx.fill();
 ctx.closePath();
 }
-function draw() {
+function draw() 
+{
 ctx.clearRect(0, 0, canvas.width, canvas.height);
-dx=startx<=20||startx>=460?dx*(-1):dx;
-dy=starty<=20||starty>=300?dy*(-1):dy;
+dx=x<=radius||x>=canvas.width-radius?dx*(-1):dx;
+dy=y<=radius||y>=canvas.height-radius?dy*(-1):dy;
+paddleX=rightPressed && paddleX+7<canvas.width-paddleWidth?paddleX+7:paddleX;
+paddleX=leftPressed && paddleX-7>0?paddleX-7:paddleX;
 drawball();
+drawpaddle();
 }
+function drawpaddle()
+{
+	ctx.beginPath();
+	ctx.rect(paddleX,canvas.height-paddleHeight,paddleWidth,paddleHeight);
+	ctx.fillStyle="#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+function keyDownHandler(e)
+{
+	console.log(0);
+ if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e)
+{
+	console.log(1);
+ if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 setInterval(draw,10);
